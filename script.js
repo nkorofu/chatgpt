604290786853cd99a784b9a1e21aff99a9a574c8
nkorofu <mitsubatti24@gmail.com>
2024-12-19 18:58:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("openButton").addEventListener("click", function() {
+    document.querySelector(".left").style.transform = "translateX(-100%)";
+    document.querySelector(".right").style.transform = "translateX(100%)";
+});
